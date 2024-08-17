5579939a4abd041f11ffdfbbed9b76d8e45fab1a
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-17 12:16:57 +0300

    feat: add some useful shortcut conventions

---
+++ nvim/lua/custom/plugins/whichkey.lua
@@ -0,0 +1,67 @@
+local status_ok, which_key = pcall(require, 'which-key')
+if not status_ok then
+  return
+end
+
+local mappings = {
+  ['t'] = {
+    "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+    'Buffers',
+  },
+  ['w'] = { '<cmd>w!<CR>', 'Save' },
+  ['q'] = { '<cmd>q!<CR>', 'Quit' },
+  ['c'] = { '<cmd>Bdelete!<CR>', 'Close Buffer' },
+  ['h'] = { '<cmd>nohlsearch<CR>', 'No Highlight' },
+  ['f'] = {
+    "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+    'Find files',
+  },
+  ['F'] = { '<cmd>Telescope live_grep theme=ivy<cr>', 'Find Text' },
+  ['P'] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", 'Projects' },
+
+  l = {
+    name = 'LSP',
+    a = { '<cmd>lua vim.lsp.buf.code_action()<cr>', 'Code Action' },
+    d = {
+      '<cmd>Telescope diagnostics bufnr=0<cr>',
+      'Document Diagnostics',
+    },
+    w = {
+      '<cmd>Telescope diagnostics<cr>',
+      'Workspace Diagnostics',
+    },
+    f = { '<cmd>lua vim.lsp.buf.format{async=true}<cr>', 'Format' },
+    i = { '<cmd>LspInfo<cr>', 'Info' },
+    I = { '<cmd>LspInstallInfo<cr>', 'Installer Info' },
+    j = {
+      '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
+      'Next Diagnostic',
+    },
+    k = {
+      '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>',
+      'Prev Diagnostic',
+    },
+    l = { '<cmd>lua vim.lsp.codelens.run()<cr>', 'CodeLens Action' },
+    q = { '<cmd>lua vim.diagnostic.setloclist()<cr>', 'Quickfix' },
+    r = { '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename' },
+    s = { '<cmd>Telescope lsp_document_symbols<cr>', 'Document Symbols' },
+    S = {
+      '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>',
+      'Workspace Symbols',
+    },
+  },
+  s = {
+    name = 'Search',
+    b = { '<cmd>Telescope git_branches<cr>', 'Checkout branch' },
+    c = { '<cmd>Telescope colorscheme<cr>', 'Colorscheme' },
+    h = { '<cmd>Telescope help_tags<cr>', 'Find Help' },
+    M = { '<cmd>Telescope man_pages<cr>', 'Man Pages' },
+    r = { '<cmd>Telescope oldfiles<cr>', 'Open Recent File' },
+    R = { '<cmd>Telescope registers<cr>', 'Registers' },
+    k = { '<cmd>Telescope keymaps<cr>', 'Keymaps' },
+    C = { '<cmd>Telescope commands<cr>', 'Commands' },
+  },
+}
+
+which_key.register(mappings)
+return {}
