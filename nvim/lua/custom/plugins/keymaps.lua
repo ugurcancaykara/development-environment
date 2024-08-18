e5771bba5e69254885a58f7a06b64106bb739ce4
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-19 00:10:53 +0300

    chore: add new keymaps for indent management and text up/down

--- nvim/lua/custom/plugins/keymaps.lua
+++ nvim/lua/custom/plugins/keymaps.lua
@@ -27,6 +27,16 @@ keymap('n', '<S-h>', ':bprevious<CR>', opts)
 --  ['x'] = { '<cmd>bufdo bd<CR>', 'Close Buffer' },
 keymap('n', '<leader>z', ':bufdo bd', opts)
 
+-- Visual --
+-- Stay in indent mode
+keymap('v', '<', '<gv^', opts)
+keymap('v', '>', '>gv^', opts)
+
+-- Move text up and down
+keymap('v', '<S-b>', ":m '>+1<CR>gv=gv", opts)
+keymap('v', '<S-n>', ":m '<-2<CR>gv=gv", opts)
+keymap('v', 'p', '"_dP', opts)
+
 -- Telescope keymaps with other plugins
 keymap('n', '<leader>ft', '<cmd>TodoTelescope<cr>', opts)
 
